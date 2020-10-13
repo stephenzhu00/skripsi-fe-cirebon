6820f6ed8068d920dca67b94679a56d1141efaae
stephenzhu00 <stephenchu.21@gmail.com>
2020-10-13 17:12:56 +0700

    interactive alerts and progress bar

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../../../shared/services/auth.service';
+import { ProgressBarService } from '../../../shared/services/progress-bar.service';
+import { AlertService } from 'ngx-alerts';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,7 @@ import { AuthService } from '../../../shared/services/auth.service';
 })
 export class LoginComponent implements OnInit {
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder, private authService:AuthService) { }
+  constructor(private formBuilder: FormBuilder, private authService:AuthService,private progressBar:ProgressBarService,private alertService: AlertService) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -22,9 +24,21 @@ export class LoginComponent implements OnInit {
   get f() {return this.form.controls; }
   // tslint:disable-next-line: typedef
   onSubmit(){
+    this.alertService.info('Checking User Info');
+    this.progressBar.startLoading();
     const myObserver = {
-      next: x => console.log('Observer got a next value: ' + x),
-      error: err => console.error('Error because ' + err),
+      next: x => {
+        this.progressBar.setSuccess();
+        this.progressBar.completeLoading();
+        console.log('Observer got a next value: ' + x);
+        this.alertService.success('You are logged in');
+      },
+      error: err => {
+        this.progressBar.setError();
+        this.progressBar.completeLoading();
+        console.error('Error because ' + err);
+        this.alertService.danger('Failed to log in');
+      },
     };
     this.authService.login(this.form.value).subscribe(myObserver);
     console.log(this.form.value);
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,23 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { ColumnOneComponent } from './layouts/column-one/column-one.component';
 import { RouterModule } from '@angular/router';
+import { NgProgressModule } from 'ngx-progressbar';
 
-
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { AlertModule } from 'ngx-alerts';
 
 @NgModule({
   declarations: [NavbarComponent, ColumnOneComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    NgProgressModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+ 
+    // Specify your library as an import (set timeout to -1 for unlimited timeout, the message can only be closed by the user clicking on it)
+    AlertModule.forRoot({maxMessages: 5, timeout: 5000, positionX: 'right', positionY: 'top'})
   ],
   exports:[
     ColumnOneComponent
