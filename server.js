dfaa5796e3c5ddab6442cbe0375ddb28a68a48f0
stephenzhu00 <stephenchu.21@gmail.com>
2020-12-04 13:14:56 +0700

    change dist

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-var app_path = '../dist/cirebon';
+var app_path = '../dist/skripsi';
 
 app.use('/',express.static(path.join(__dirname,app_path)))
 .get('*',(req,res)=>res.sendFile(path.join(__dirname,app_path +'/index.html')))
